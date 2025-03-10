9ff0e1733bbb09aa958e20e0edbb569e6d0b1ec9
prakash sirvi <prakash.sirvi@metacube.com>
2025-03-10 10:22:23 +0530

    chages

--- Area.java
+++ Area.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 
 public class Area{
     
-      public double triangle_area(double h , double w){
+      public double triangleArea(double h , double w){
         if(h<=0 || w<=0){
              throw  new ArithmeticException("Height and Width must be greater then 0") ; 
          }
              return (h*w)/2 ; 
       }
-      public double rectangle_area(double h, double w){
+      public double rectangleArea(double h, double w){
         if(h<=0 || w<=0){
              throw  new ArithmeticException("Height and Width must be greater then 0") ; 
          }
            return  (h*w) ;
       }
 
-      public double squre_area(double w){
+      public double squreAarea(double w){
         if(w<=0){
              throw  new ArithmeticException("Width must be greater then 0") ; 
          }
          return  w*w ;
       }
 
-      public double circle_area(double r){
+      public double circleArea(double r){
         if(r<=0){
              throw  new ArithmeticException("Radius must be greater then 0") ; 
          }
@@ -36,7 +36,7 @@ public class Area{
         Scanner sc = new Scanner(System.in) ;   
         System.out.println("Enter the width ")  ; 
             int width = sc.nextInt() ; 
-        System.out.println("Enter the Height ") ;
+        System.out.println("Enter the Heigth ") ;
            int height = sc.nextInt() ; 
 
         System.out.println("Enter the Radius ") ; 
@@ -44,10 +44,10 @@ public class Area{
         
         try{ 
              
-        System.out.println("Triangle  area : "+ a.triangle_area(height ,width)) ; 
-        System.out.println("Rectangle area : " +a.rectangle_area(height, width)) ;
-        System.out.println("Square area : " + a.squre_area(width)) ; 
-        System.out.println("Circle are : " + a.circle_area(radius)) ;
+        System.out.println("Triangle  area : "+ a.triangleArea(height ,width)) ; 
+        System.out.println("Rectangle area : " +a.rectangleArea(height, width)) ;
+        System.out.println("Square area : " + a.squreArea(width)) ; 
+        System.out.println("Circle are : " + a.circleArea(radius)) ;
         }
         catch(ArithmeticException e){
              System.out.println("Error - " +e.getMessage()) ;
