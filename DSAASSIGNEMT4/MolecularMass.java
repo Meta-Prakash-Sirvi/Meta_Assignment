d39e4df0ed96060e57a50dd4062cbd531249aef9
Meta-Prakash-Sirvi <prakash.sirvi@metacube.com>
2025-04-08 10:28:54 +0530

    Add files via upload

--- DSAASSIGNEMT4/MolecularMass.java
+++ DSAASSIGNEMT4/MolecularMass.java
@@ -14,21 +14,28 @@ public class MolecularMass {
         molecularMasss.put('O', 16);
     }
 
-    public static int solve(String input) {
+    public static int findMolecularMass(String input) {
         int size = input.length();
         int result = 0;
         int index = 0;
         while (index < size) {
             char ch = input.charAt(index);
-
+            char nextch = (index + 1 < input.length()) ? input.charAt(index + 1) : 0;
             if (ch >= 65 && ch <= 90) {
                 massWeight.push(molecularMasss.get(ch));
+            } else if ((ch >= '1' && ch <= '9') && (nextch >= '0' && nextch <= '9')) {
+
+                int element = massWeight.pop();
+                massWeight.push(element * (10 * ((int) (ch - '0')) + ((int) (nextch - '0'))));
+                index++;
             } else if (ch >= '1' && ch <= '9') {
                 int element = massWeight.pop();
                 element = element * ((int) ch - '0');
                 massWeight.push(element);
+
             } else if (ch == '(') {
                 massWeight.push(-1);
+
             } else {
                 int temp = 0;
                 while (!massWeight.isEmpty() && massWeight.peek() != -1) {
@@ -41,7 +48,9 @@ public class MolecularMass {
         }
 
         while (!massWeight.isEmpty()) {
+
             result += massWeight.pop();
+
         }
         return result;
     }
@@ -51,6 +60,6 @@ public class MolecularMass {
         System.out.println("Enter the  organic chemistry formula");
         String input = sc.next();
 
-        System.out.println("Molecular Mass : " + solve(input));
+        System.out.println("Molecular Mass : " + findMolecularMass(input));
     }
 }
