33b6169b6b895c53f9709354c235006ef8b7bf9e
Meta-Prakash-Sirvi <prakash.sirvi@metacube.com>
2025-04-09 11:16:50 +0530

    Add files via upload

--- DSAASSIGNEMT4/EmployeeInfomation.java
+++ DSAASSIGNEMT4/EmployeeInfomation.java
@@ -1,102 +1,99 @@
-package DSAASSIGNEMT4 ; 
+package DSAASSIGNEMT4;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Scanner;
 
-class Employee{
+class Employee {
     private int empId;
     private String name;
     private String address;
 
-    Employee(int empId, String name, String address){
+    static private ArrayList<Employee> empList = new ArrayList<>();
+
+    Employee(int empId, String name, String address) {
         this.empId = empId;
         this.name = name;
         this.address = address;
     }
 
-    int getId(){
-         return empId ; 
-    }
-
-    String getName(){
-         return name ;
-    }
-
-    String getAddress(){
-        return address ;
-   }
-}
-
-class Operation{
-    private ArrayList<Employee> empList;
-    private HashSet<Integer> empIdSet;
-    Operation(){
-        empIdSet = new HashSet<>();
-        empList = new ArrayList<>();
-    }
-
-    void neturalSort(){
+    void neturalSort() {
         empList.sort(Comparator.comparingInt(e -> e.getId()));
     }
 
-    void sortingByName(){
+    void sortingByName() {
         empList.sort(Comparator.comparing(e -> e.getName()));
     }
 
-    boolean CheckEmployee(int id){
-        if(!empIdSet.contains(id)){
-            return true ; 
+    boolean CheckEmployee(int id) {
+        for (Employee emp : empList) {
+            if (id == emp.empId) {
+                return false;
+            }
         }
-        return false;
-    }
-    
-    void addEmployee(Employee emp){
-         empList.add(emp) ; 
-         empIdSet.add(emp.getId()) ; 
+        return true;
     }
 
-    void dispaly(){
-        for(Employee emp : empList){
-            System.out.println(emp.getId()+" "+emp.getName()+" "+emp.getAddress());
+    void addEmployee(Employee emp) {
+        empList.add(emp);
+    }
+
+    int getId() {
+        return empId;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getAddress() {
+        return address;
+    }
+
+    void dispaly() {
+        for (Employee emp : empList) {
+            System.out.println(emp.getId() + " " + emp.getName() + " " + emp.getAddress());
         }
     }
 }
 
 public class EmployeeInfomation {
     public static void main(String[] args) {
-        Operation operation = new Operation();
+        // Operation operation = new Operation();
+        Employee emp ;
+        
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("enter the employee id ");
+            int empId = sc.nextInt();
+            System.out.println("enter the employee name ");
+            String empName = sc.next();
+            System.out.println("enter the employee address");
+            String empAddress = sc.next();
+            emp = new Employee(empId, empName, empAddress);
 
-        Scanner sc = new Scanner(System.in) ; 
-        while(true){
-             System.out.println("enter the employee id ");
-             int empId = sc.nextInt() ; 
-             System.out.println("enter the employee name ");
-             String empName = sc.next() ; 
-             System.out.println("enter the employee address");
-                String empAddress = sc.next() ; 
-                Employee emp = new Employee(empId, empName, empAddress) ; 
-            if(operation.CheckEmployee(emp.getId())){
-                 operation.addEmployee(emp);
-            }else{
-                 System.out.println("duplicate employee id , skipp...");
+            if (emp.CheckEmployee(emp.getId())) {
+                emp.addEmployee(emp);
+            } else {
+                System.out.println("duplicate employee id , skipp...");
             }
 
             System.out.println("Press -1 exit");
-            int press = sc.nextInt() ; 
-            if(press==-1){
-                 break ; 
+            int press = sc.nextInt();
+            if (press == -1) {
+                break;
             }
 
-
         }
-        
-        System.out.println("sorting according employee ID ");
-        operation.neturalSort();
-        operation.dispaly();
 
-        System.out.println("sorting accoding employee name ");
-        operation.sortingByName();
-        operation.dispaly();
+        System.out.println("sorting according employee ID ");
+        emp.neturalSort();
+        emp.dispaly();
+
+        // System.out.println("sorting accoding employee name ");
+        // operation.sortingByName();
+        // operation.dispaly();
     }
+
 }
