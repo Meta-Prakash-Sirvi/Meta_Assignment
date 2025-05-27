28c24222cb6037f1a022d169171f34c258baa3f9
Meta-Prakash-Sirvi <prakash.sirvi@metacube.com>
2025-05-27 15:37:36 +0530

    Add files via upload

---
+++ python_dataType/datatype.py
@@ -0,0 +1,14 @@
+firstValue = 10
+secondValue = 20.54
+check = True
+string  = "Python is Awesome"
+
+print(firstValue)
+print(secondValue)
+print(check)
+print(string)
+
+print(type(firstValue))
+print(type(secondValue))
+print(type(check))
+print(type(string))
